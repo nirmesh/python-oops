abb775b14a58044b5e5a10e9eabd416e7000a379
d930379 <you@example.com>
2021-04-25 15:01:55 +0530

    added examples

--- list.py
+++ list.py
@@ -1,25 +1,42 @@
 subjects=['physics','math']
 games=['chess','cricket']
-print(subjects*2 +games);
+#print(subjects*2 +games);
 
-print(subjects[0:5])# doesn't matter if u are asking more
+#print(subjects[0:5])# doesn't matter if u are asking more
 
 # tuples are immutable u can't append etc
 
-football=('a','b','c')
-cricket=('x','y','z');
+# football=('a','b','c')
+# cricket=('x','y','z');
+#
+#
+# print(football[1]);
+# print(football.index('c'))
+#
+# print(football[0:2])
+# print(football.count('a'))
+#
+#
+# players=[('a','b','c'),('x','y','z')]
+# print(players[1][2]);
+#
 
 
-print(football[1]);
-print(football.index('c'))
+str="hello nirmesh"
+print(str.__len__());
 
-print(football[0:2])
-print(football.count('a'))
+print(str.index('n'))
 
+print(str[::-1]) # reverse ur string
 
-players=[('a','b','c'),('x','y','z')]
-print(players[1][2]);
+if "n" in str:
+    print("element  present")
 
+#it is case sensitive so wont print anything
+if "N" in str:
+    print("element  not present")
 
+#another way  is not in
 
-
+if "N" not in str:
+    print("capital N not present")
---
+++ sets.py
@@ -0,0 +1,13 @@
+Integer={1,2,3,45};
+
+Integer.discard(45)
+print(Integer);
+
+real={5,6,2,9}
+
+print(Integer|real)
+
+print(Integer-real) # removes common and all in real and prints remaining part in integer
+
+print(Integer^real) # union of uncommon elements
+
